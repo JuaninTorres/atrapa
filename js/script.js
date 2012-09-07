f66df2dd9eade447fcafca08d87079200c886814
Juan Torres <juanin.torres@gmail.com>
2012-09-07 08:04:17 -0300

    Algunos cambios

--- js/script.js
+++ js/script.js
@@ -2,66 +2,46 @@
 
 */
 
-var tiempoGeneral = parseInt(0,10);
-var cronometroEnEjecucion = false;
-var resultado = [];
+var segundos = 0;
+var milisec = 0;
+var countdownEnEjecucion=false;
+var modalOpen=false;
 
-function formatearTiempo(t){
-    // El tiempo viene en milisegundos
-    var m_tmp   = parseInt(t,10);
-    var horas   = Math.floor(m_tmp/ 3600000);
-    var minutos = Math.floor((m_tmp - (horas * 3600000)) / 60000);
-    var segundos = Math.floor((m_tmp - (horas * 3600000) - (minutos * 60000))/1000);
-    var milisegundos = ((m_tmp - (horas * 3600000) - (minutos * 60000)) - (segundos * 1000) )/100;
-
-    if (horas   < 10) {horas   = "0"+horas;}
-    if (minutos < 10) {minutos = "0"+minutos;}
-    if (segundos < 10) {segundos = "0"+segundos;}
-    var time    = horas+':'+minutos+':'+segundos+'.'+milisegundos;
-    return time;
-}
-
-function removeA(arr){
-    var what, a= arguments, L= a.length, ax;
-    while(L> 1 && arr.length){
-        what= a[--L];
-        while((ax= arr.indexOf(what))!= -1){
-            arr.splice(ax, 1);
-        }
-    }
-    return arr;
-}
-
-function cronometroPrincipal()
-{
-    $("#cronometroPrincipal").html(formatearTiempo(tiempoGeneral));
-    if(cronometroEnEjecucion)
-    {
-        tiempoGeneral = tiempoGeneral+100;
-        setTimeout("cronometroPrincipal()",100);
+function loadDisplayCountDown(seg){
+    
+    if(!countdownEnEjecucion){
+        segundos = seg;
+        countdownEnEjecucion = true;
+        displayCountDown();
     }
 }
 
-function detenerRelojEquipo(id){
-    $("#reloj"+id).html(formatearTiempo(tiempoGeneral));
-    $("#tiempo"+id).val(tiempoGeneral);
+function displayCountDown(){ 
 
-    if($.inArray(id,resultado)!=-1){
-        removeA(resultado,id);
-    }
-    resultado.push(id);
-}
 
-function iniciarCronometro(ejecutando){
-    if(!ejecutando)
-    {
-        cronometroEnEjecucion=true;
-        cronometroPrincipal();
+ if (milisec<=0){ 
+    milisec=9;
+    segundos-=1; 
+ } 
+ if (segundos<=-1){ 
+    milisec=0;
+    segundos=0;
+    $("#tiempoEspera").html("Ahora ya puede autentificarse");
+    $("#tiempoEspera").removeClass("tiempoEspera").addClass("tiempoEsperaOK");
+    $("#comentarioTiempoEspera").hide();
+    countdownEnEjecucion = false;
+    return true;
+    
+ } 
+ else 
+    milisec-=1 
+    var mensaje = segundos+"."+milisec
+    $("#tiempoEspera").html(mensaje);
+    if (modalOpen){
+        setTimeout("displayCountDown()",100);
     }
-    else
-    {
-        cronometroEnEjecucion=false;
-    }
-}
-function detenerCronometro(){$("#btnIniciar").removeAttr("disabled");cronometroEnEjecucion=false;}
-function resetCronometro(){$("#btnIniciar").removeAttr("disabled");cronometroEnEjecucion=false;tiempoGeneral=0;cronometroPrincipal();}
+       
+    
+    //displayCountDown(); 
+} 
+ 
